dac54d71427168561e8b9a75894bf4c3790d413c
juanmi <juanmi-64@hotmail.com>
2024-02-15 11:57:44 +0100

    Creo la clase Modelo y paso los tests

---
+++ src/main/java/org/iesalandalus/programacion/tallermecanico/modelo/Modelo.java
@@ -0,0 +1,124 @@
+package org.iesalandalus.programacion.tallermecanico.modelo;
+
+import org.iesalandalus.programacion.tallermecanico.modelo.dominio.Cliente;
+import org.iesalandalus.programacion.tallermecanico.modelo.dominio.Revision;
+import org.iesalandalus.programacion.tallermecanico.modelo.dominio.Vehiculo;
+import org.iesalandalus.programacion.tallermecanico.modelo.negocio.Clientes;
+import org.iesalandalus.programacion.tallermecanico.modelo.negocio.Revisiones;
+import org.iesalandalus.programacion.tallermecanico.modelo.negocio.Vehiculos;
+
+import javax.naming.OperationNotSupportedException;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+public class Modelo {
+    private Clientes clientes;
+    private Revisiones revisiones;
+    private Vehiculos vehiculos;
+
+    public Modelo() {}
+    public void comenzar() {
+        clientes = new Clientes();
+        revisiones = new Revisiones();
+        vehiculos = new Vehiculos();
+    }
+
+    public void terminar() {
+        System.out.printf("El modelo ha terminado. %n");
+    }
+
+    public void insertar(Cliente cliente) throws OperationNotSupportedException {
+        clientes.insertar(new Cliente(cliente));
+    }
+
+    public void insertar(Vehiculo vehiculo) throws OperationNotSupportedException {
+        vehiculos.insertar(vehiculo);
+    }
+
+    public void insertar(Revision revision) throws OperationNotSupportedException {
+        revisiones.insertar(new Revision(buscar(revision.getCliente()), buscar(revision.getVehiculo()), revision.getFechaInicio()));
+    }
+
+    public Cliente buscar(Cliente cliente) {
+        return new Cliente(clientes.buscar(cliente));
+    }
+
+    public Vehiculo buscar(Vehiculo vehiculo) {
+        return vehiculos.buscar(vehiculo);
+    }
+
+    public Revision buscar(Revision revision) {
+        return new Revision(revisiones.buscar(revision));
+    }
+
+    public boolean modificar(Cliente cliente, String nombre, String telefono) throws OperationNotSupportedException {
+        return clientes.modificar(cliente, nombre, telefono);
+    }
+
+    public void anadirHoras(Revision revision, int horas) throws OperationNotSupportedException {
+        revisiones.anadirHoras(revision, horas);
+    }
+    public void anadirPrecioMaterial(Revision revision, float precioMaterial) throws OperationNotSupportedException {
+        revisiones.anadirPrecioMaterial(revision, precioMaterial);
+    }
+
+    public void cerrar(Revision revision, LocalDate fechaFin) throws OperationNotSupportedException {
+        revisiones.cerrar(revision, fechaFin);
+    }
+
+    public void borrar(Cliente cliente) throws OperationNotSupportedException {
+        for (Revision revision : revisiones.get(cliente)) {
+            borrar(revision);
+        }
+        clientes.borrar(cliente);
+    }
+
+    public void borrar(Vehiculo vehiculo) throws OperationNotSupportedException {
+        for (Revision revision : revisiones.get(vehiculo)) {
+            borrar(revision);
+        }
+        vehiculos.borrar(vehiculo);
+    }
+
+    public void borrar(Revision revision) throws OperationNotSupportedException {
+        revisiones.borrar(revision);
+    }
+
+    public List<Cliente> getClientes() {
+        List<Cliente> nuevosClientes = new ArrayList<>();
+        for (Cliente cliente : clientes.get()) {
+            nuevosClientes.add(new Cliente(cliente));
+        }
+        return nuevosClientes;
+    }
+
+    public List<Vehiculo> getVehiculos() {
+        return new ArrayList<>(vehiculos.get());
+    }
+
+    public List<Revision> getRevisiones() {
+        List<Revision> nuevasRevisiones = new ArrayList<>();
+        for (Revision revision : revisiones.get()) {
+            nuevasRevisiones.add(new Revision(revision));
+        }
+        return nuevasRevisiones;
+    }
+
+    public List<Revision> getRevisiones(Cliente cliente) {
+        List<Revision> nuevasRevisiones = new ArrayList<>();
+        for (Revision revision : revisiones.get(cliente)) {
+            nuevasRevisiones.add(new Revision(revision));
+        }
+        return nuevasRevisiones;
+    }
+
+    public List<Revision> getRevisiones(Vehiculo vehiculo) {
+        List<Revision> nuevasRevisiones = new ArrayList<>();
+        for (Revision revision : revisiones.get(vehiculo)) {
+            nuevasRevisiones.add(new Revision(revision));
+        }
+        return nuevasRevisiones;
+    }
+}
