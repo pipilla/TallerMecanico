83a1501da10911f6a47aa57808302f4761c7c389
juanmi <juanmi-64@hotmail.com>
2024-03-18 12:55:43 +0100

    Cambios del día 18/03/2024

--- src/main/java/org/iesalandalus/programacion/tallermecanico/controlador/Controlador.java
+++ src/main/java/org/iesalandalus/programacion/tallermecanico/controlador/Controlador.java
@@ -35,11 +35,49 @@ public class Controlador implements IControlador {
                     modelo.insertar(vista.leerCliente());
                     resultado = "Se ha insertado el cliente correctamente.";
                 }
+                case BUSCAR_CLIENTE -> {
+                    modelo.buscar(vista.leerCliente());
+                    resultado = "Se ha encontrado el cliente.";
+                }
+                case BORRAR_CLIENTE -> {
+                }
+                case LISTAR_CLIENTES -> {
+                }
+                case MODIFICAR_CLIENTE -> {
+                }
+                case INSERTAR_VEHICULO -> {
+                }
+                case BUSCAR_VEHICULO -> {
+                }
+                case BORRAR_VEHICULO -> {
+                }
+                case LISTAR_VEHICULOS -> {
+                }
+                case INSERTAR_REVISION -> {
+                }
+                case BUSCAR_REVISION -> {
+                }
+                case BORRAR_REVISION -> {
+                }
+                case LISTAR_REVISIONES -> {
+                }
+                case LISTAR_REVISIONES_CLIENTE -> {
+                }
+                case LISTAR_REVISIONES_VEHICULO -> {
+                }
+                case ANADIR_HORAS_REVISION -> {
+                }
+                case ANADIR_PRECIO_MATERIAL_REVISION -> {
+                }
+                case CERRAR_REVISION -> {
+                }
+                case SALIR -> {
+                }
             }
-
+            vista.notificarResultado(evento,resultado,true);
         } catch (Exception e) {
-
+            resultado = e.getMessage();
+            vista.notificarResultado(evento,resultado,false);
         }
-
     }
 }
