08275acc2222fee159185ee6493e5d83edaf2e71
juanmi <juanmi-64@hotmail.com>
2024-05-10 13:06:09 +0200

    Creo el paquete de vista gráfica y la clase VistaGrafica

---
+++ src/main/java/org/iesalandalus/programacion/tallermecanico/vista/grafica/VistaGrafica.java
@@ -0,0 +1,136 @@
+package org.iesalandalus.programacion.tallermecanico.vista.grafica;
+
+import org.iesalandalus.programacion.tallermecanico.modelo.dominio.Cliente;
+import org.iesalandalus.programacion.tallermecanico.modelo.dominio.TipoTrabajo;
+import org.iesalandalus.programacion.tallermecanico.modelo.dominio.Trabajo;
+import org.iesalandalus.programacion.tallermecanico.modelo.dominio.Vehiculo;
+import org.iesalandalus.programacion.tallermecanico.vista.Vista;
+import org.iesalandalus.programacion.tallermecanico.vista.eventos.Evento;
+import org.iesalandalus.programacion.tallermecanico.vista.eventos.GestorEventos;
+
+import javax.naming.OperationNotSupportedException;
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Map;
+
+public class VistaGrafica implements Vista {
+    @Override
+    public GestorEventos getGestorEventos() {
+        return null;
+    }
+
+    @Override
+    public void comenzar() {
+
+    }
+
+    @Override
+    public void terminar() {
+
+    }
+
+    @Override
+    public Cliente leerCliente() {
+        return null;
+    }
+
+    @Override
+    public Cliente leerClienteDni() {
+        return null;
+    }
+
+    @Override
+    public String leerNuevoNombre() {
+        return null;
+    }
+
+    @Override
+    public String leerNuevoTelefono() {
+        return null;
+    }
+
+    @Override
+    public Vehiculo leerVehiculo() {
+        return null;
+    }
+
+    @Override
+    public Vehiculo leerVehiculoMatricula() {
+        return null;
+    }
+
+    @Override
+    public Trabajo leerRevision() {
+        return null;
+    }
+
+    @Override
+    public Trabajo leerMecanico() {
+        return null;
+    }
+
+    @Override
+    public Trabajo leerTrabajoVehiculo() {
+        return null;
+    }
+
+    @Override
+    public int leerHoras() throws OperationNotSupportedException {
+        return 0;
+    }
+
+    @Override
+    public float leerPrecioMaterial() throws OperationNotSupportedException {
+        return 0;
+    }
+
+    @Override
+    public LocalDate leerFechaCierre() throws OperationNotSupportedException {
+        return null;
+    }
+
+    @Override
+    public LocalDate leerMes() {
+        return null;
+    }
+
+    @Override
+    public void notificarResultado(Evento evento, String texto, boolean exito) {
+
+    }
+
+    @Override
+    public void mostrarCliente(Cliente cliente) {
+
+    }
+
+    @Override
+    public void mostrarVehiculo(Vehiculo vehiculo) {
+
+    }
+
+    @Override
+    public void mostrarTrabajo(Trabajo trabajo) {
+
+    }
+
+    @Override
+    public void mostrarClientes(List<Cliente> clientes) {
+
+    }
+
+    @Override
+    public void mostrarVehiculos(List<Vehiculo> vehiculos) {
+
+    }
+
+    @Override
+    public void mostrarTrabajos(List<Trabajo> trabajos) {
+
+    }
+
+    @Override
+    public void mostrarEstadisticasMensuales(Map<TipoTrabajo, Integer> estadisticas) {
+
+    }
+}
