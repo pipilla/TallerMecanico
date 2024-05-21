db2fd925a6e4f709f12e730a7b955a0a50d3f86b
juanmi <juanmi-64@hotmail.com>
2024-05-21 11:03:33 +0200

    Cambios para la implementación de MongoDB

--- src/main/java/org/iesalandalus/programacion/tallermecanico/modelo/negocio/mongodb/Vehiculos.java
+++ src/main/java/org/iesalandalus/programacion/tallermecanico/modelo/negocio/mongodb/Vehiculos.java
@@ -1,6 +1,8 @@
 package org.iesalandalus.programacion.tallermecanico.modelo.negocio.mongodb;
 
 import com.mongodb.client.MongoCollection;
+import com.mongodb.client.model.FindOneAndReplaceOptions;
+import com.mongodb.client.result.DeleteResult;
 import org.bson.Document;
 import org.bson.conversions.Bson;
 import org.iesalandalus.programacion.tallermecanico.modelo.dominio.Cliente;
@@ -8,7 +10,9 @@ import org.iesalandalus.programacion.tallermecanico.modelo.dominio.Vehiculo;
 import org.iesalandalus.programacion.tallermecanico.modelo.negocio.IVehiculos;
 
 import javax.naming.OperationNotSupportedException;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 import static com.mongodb.client.model.Filters.eq;
 
@@ -66,21 +70,35 @@ public class Vehiculos implements IVehiculos {
 
     @Override
     public List<Vehiculo> get() {
-        return null;
+        List<Vehiculo> vehiculos = new ArrayList<>();
+        for (Document documento : coleccionVehiculos.find()) {
+            vehiculos.add(getVehiculo(documento));
+        }
+        return vehiculos;
     }
 
     @Override
     public void insertar(Vehiculo vehiculo) throws OperationNotSupportedException {
-
+        Objects.requireNonNull(vehiculo, "No se puede insertar un vehículo nulo.");
+        FindOneAndReplaceOptions opciones = new FindOneAndReplaceOptions().upsert(true);
+        Document resultado = coleccionVehiculos.findOneAndReplace(getCriterioBusqueda(vehiculo), getDocumento(vehiculo), opciones);
+        if (resultado != null) {
+            throw new OperationNotSupportedException("Ya existe un vehículo con esa matrícula.");
+        }
     }
 
     @Override
     public Vehiculo buscar(Vehiculo vehiculo) {
-        return null;
+        Objects.requireNonNull(vehiculo, "No se puede buscar un vehículo nulo.");
+        return getVehiculo(coleccionVehiculos.find(getCriterioBusqueda(vehiculo)).first());
     }
 
     @Override
     public void borrar(Vehiculo vehiculo) throws OperationNotSupportedException {
-
+        Objects.requireNonNull(vehiculo, "No se puede borrar un vehículo nulo.");
+        DeleteResult resultado = coleccionVehiculos.deleteOne(getCriterioBusqueda(vehiculo));
+        if (resultado.getDeletedCount() == 0) {
+            throw new OperationNotSupportedException("No existe ningún vehículo con esa matrícula.");
+        }
     }
 }
